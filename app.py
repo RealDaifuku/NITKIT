791ed949a8b196215ab497d741c843b9c2db57e3
RealDaifuku <pakkanangt@gmail.com>
2026-05-12 11:30:18 +0700

    Third push for Render

--- app.py
+++ app.py
@@ -1,45 +1,53 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request, jsonify
 from ultralytics import YOLO
 import cv2
+import numpy as np
+import base64
 
 app = Flask(__name__)
 
-# Load model
+# Load YOLO model
 model = YOLO("runs/detect/train-12/weights/best.pt")
 
-# Open webcam
-cap = cv2.VideoCapture(0)
 
-def generate_frames():
-    while True:
-        success, frame = cap.read()
-
-        if not success:
-            break
-
-        # YOLO prediction
-        results = model(frame)
-
-        # Draw boxes
-        annotated_frame = results[0].plot()
-
-        # Encode frame as jpg
-        ret, buffer = cv2.imencode('.jpg', annotated_frame)
-
-        frame_bytes = buffer.tobytes()
-
-        # Stream frame
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.json["image"]
+
+    # Remove header
+    image_data = data.split(",")[1]
+
+    # Decode base64 image
+    image_bytes = base64.b64decode(image_data)
+
+    # Convert to numpy array
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+
+    # Decode image
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    # YOLO prediction
+    results = model(frame)
+
+    # Draw boxes
+    annotated_frame = results[0].plot()
+
+    # Encode back to jpg
+    _, buffer = cv2.imencode(".jpg", annotated_frame)
+
+    # Convert to base64
+    processed_image = base64.b64encode(buffer).decode("utf-8")
+
+    return jsonify({
+        "image": processed_image
+    })
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
